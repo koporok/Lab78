d0f488771a819eaf5d15584409143932faf13cad
koporok <122740702+koporok@users.noreply.github.com>
2024-04-16 09:44:42 +0300

    Add files via upload

---
+++ Lab7/Lab8.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    using System;
+
+    class UserLoginArray : GenericArray<string> 
+    {
+        public UserLoginArray(int size) : base(size) 
+        {
+        }
+
+        public void RegisterUser(string login) 
+        {
+            Add(login); 
+        }
+    }
+
+    class UserPasswordArray : GenericArray<string> 
+    {
+        public UserPasswordArray(int size) : base(size) 
+        {
+        }
+
+        public void SetPassword(int index, string password) 
+        {
+            if (index >= 0 && index < GetLength())
+            {
+                array[index] = password;
+            }
+            else
+            {
+                //throw new IndexOutOfRangeException("Индекс находится вне диапазона");
+            }
+        }
+    }
+
+    class Lab8
+    {
+        static void Main()
+        {
+            UserLoginArray loginArray = new UserLoginArray(5); 
+            loginArray.RegisterUser("Лол");
+            loginArray.RegisterUser("Лилпоп");
+
+            UserPasswordArray passwordArray = new UserPasswordArray(5); 
+            passwordArray.SetPassword(0, "123456789");
+            passwordArray.SetPassword(1, "987654321");
+            //Console.WriteLine("8lab");
+        }
+    }
+
+}
---
+++ Lab7/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class GenericArray<T>
+{
+    public T[] array;
+    private int length;
+
+    public GenericArray(int size)
+    {
+        array = new T[size];
+        length = 0;
+    }
+
+    public void Add(T element)
+    {
+        array[length] = element;
+        length++;
+        Console.WriteLine(length);
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= length)
+        {
+            throw new Exception("Индекс находится вне диапазона");
+        }
+
+        for (int i = index; i < length - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+
+        length--;
+        Console.WriteLine($"Удаление {length}");
+    }
+
+    public T GetElementAt(int index)
+    {
+        if (index < 0 || index >= length)
+        {
+            throw new Exception("Индекс находится вне диапазона");
+        }
+
+
+        Console.WriteLine(array[index]);
+        return array[index];
+    }
+
+    public int GetLength()
+    {
+        return length;
+    }
+}
+
+/*class Program
+{
+    static void Main()
+    {
+        GenericArray<int> intArray = new GenericArray<int>(5);
+        intArray.Add(1);
+        intArray.Add(2);
+        intArray.Add(3);
+
+        GenericArray<string> stringArray = new GenericArray<string>(3);
+        stringArray.Add("Hello");
+        stringArray.Add("World");
+
+        GenericArray<double> doubleArray = new GenericArray<double>(4);
+        doubleArray.Add(1.5);
+        doubleArray.Add(2.5);
+        doubleArray.Add(3.5);
+    }
+}*/
